e624014e03a7cdeee1b0612a1a8982e95f6eea6d
v-max07 <vusandebnath09@gmail.com>
2022-05-23 02:36:38 +0600

    add about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './About.css';
+import Image from '../../images/me.jpg';
 
 const About = () => {
     return (
-        <div>
-            <h2>About!</h2>
+        <div className='p-5 vh-100'>
+            <img width={150} src={Image} alt="" />
+            <h2>S.M. Vusan Debnath</h2>
+            <h3>My Goal</h3>
+            <p>My goal is to become a full-stack web developer within the next three months so I am willing to work hard and I will.</p>
         </div>
     );
 };
